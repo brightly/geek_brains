9b86f41cfba65a3fe05adcbe457368168b252458
irabrightly <ivinnikova@gmail.com>
2021-03-11 20:29:43 +0300

    Дз №4

--- task_4_2.py
+++
@@ -1,3 +0,0 @@
-l_first = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55]
-l_last = [l_first[el] for el in range(1, len(l_first)) if l_first[el] > l_first[el - 1]]
-print(l_last)
--- task_4_4.py
+++
@@ -1,3 +0,0 @@
-list_n = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11]
-new_list = [el for el in list_n if list_n.count(el) == 1]
-print(new_list)
